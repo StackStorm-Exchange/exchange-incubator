53d5629167933d530713149f1bb223db1dc0546f
Yugendra <yugendra.khonde@gslab.com>
2017-04-28 20:48:13 +0530

    remived linting error from actions publish_configs publish_configs_day1

--- nsx_vtep/actions/publish_configs.py
+++ nsx_vtep/actions/publish_configs.py
@@ -12,16 +12,15 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-from NSX.nsx import NSX
 from st2actions.runners.pythonrunner import Action
 
 class AddHardwareVtep(Action):
     def __init__(self, config=None, action_service=None):
         super(AddHardwareVtep, self).__init__(config=config, action_service=action_service)
-        
+
     def run(self):
         configs = {}
-        
+
         configs['lswitch_name'] = self.config['lswitch_name']
         configs['nsx_mgr_ip'] = self.config['nsx_mgr_ip']
         configs['nsx_mgr_pass'] = self.config['nsx_mgr_pass']
@@ -30,7 +29,5 @@ class AddHardwareVtep(Action):
         configs['switch_name'] = self.config['switch_name']
         configs['vlan'] = self.config['vlan']
         configs['vtep_name'] = self.config['vtep_name']
-        
+
         return configs
-        
-        

f16120e9ee2a75141b1c4aa809d2aa3c932931b3
Yugendra <yugendra.khonde@gslab.com>
2017-04-28 12:29:01 +0530

    removed linting errors in action attach_vlan_to_gw

--- vdx_vtep/actions/attach_vlan_to_gw.py
+++ vdx_vtep/actions/attach_vlan_to_gw.py
@@ -12,21 +12,22 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
+import logging
 from netmiko import ConnectHandler
 import pynos.device
 import pynos.utilities
-import logging
 from st2actions.runners.pythonrunner import Action
 
 class attachVlanToGw(Action):
     def __init__(self, config=None):
-        super(attachVlanToGw, self).__init__(config=config)
-        
+        Action.__init__(self, config=config)
+        self.logger = logging.getLogger(__name__)
+
     def run(self, host=None, username=None, password=None, overlay_gateway_name=None, vlan=None):
         """Run helper methods to implement the desired state.
         """
         if host is None:
-            self._host = self.config['vip']
+            host = self.config['vip']
 
         if username is None:
             username = self.config['username']
@@ -36,8 +37,6 @@ class attachVlanToGw(Action):
 
         if overlay_gateway_name is None:
             overlay_gateway_name = self.config['overlay_gateway_name']
-
-        self.logger = logging.getLogger(__name__)
 
         if vlan is None:
             vlan = self.config['vlan']
@@ -54,12 +53,14 @@ class attachVlanToGw(Action):
         if changes['pre_requisites']:
             changes['attach_vlan'] = self._attach_vlan(host, auth, overlay_gateway_name, vlan)
         else:
-            self.logger.info('Pre-requisites validation failed while attaching VLAN to overlay gateway')
+            self.logger.info('Pre-requisites validation failed while attaching VLAN \
+                to overlay gateway')
         if not changes['attach_vlan']:
             self.logger.info('Attach VLAN to overlay gateway has failed.')
             exit(1)
         else:
-            self.logger.info('closing connection to %s after attaching VLAN to overlay gateway' % host)
+            self.logger.info('closing connection to %s after attaching VLAN \
+                to overlay gateway', host)
             return changes
 
     def _check_requirements(self, dev, overlay_gateway_name, vlan):
@@ -72,22 +73,23 @@ class attachVlanToGw(Action):
         for each_vlan in output:
             if 'vlan-id' in each_vlan and each_vlan['vlan-id'] == str(vlan):
                 is_vlan_exist = True
-        if not (is_vlan_exist):
+        if not is_vlan_exist:
             raise ValueError("VlanId %s does not exists" % vlan)
 
         # logic to check if overlay-Gateway is already present and vlan is already attached.
         result = dev.hw_vtep.get_overlay_gateway()
-        if (not result):
+        if not result:
             self.logger.info(' No Overlay-Gateway configured on the device')
             return False
         else:
             if result['name'] == str(overlay_gateway_name):
                 if 'attached-vlan' in result:
-                    if (str(vlan) in result['attached-vlan']):
-                        self.logger.info('VLAN: %s is already attached to overlay gateway.' % vlan)
+                    if str(vlan) in result['attached-vlan']:
+                        self.logger.info('VLAN: %s is already attached to overlay gateway.', vlan)
                         return False
             else:
-                self.logger.info('Overlay gateway: %s not configured on the switch.' % overlay_gateway_name)
+                self.logger.info('Overlay gateway: %s not configured on the switch.', \
+                    overlay_gateway_name)
                 return False
         return True
 
@@ -99,11 +101,7 @@ class attachVlanToGw(Action):
         cmd1 = "overlay-gateway %s" % (overlay_gateway_name)
         cmd2 = "attach vlan %d" % (int(vlan))
         cmds = ['configure', cmd1, cmd2]
-        result = self._execute_cmd(host, auth, cmds)
-        if result:
-            return True
-        else:
-            return False
+        return bool(self._execute_cmd(host, auth, cmds))
 
     def _execute_cmd(self, host, auth, cli_cmd):
         '''
@@ -121,13 +119,12 @@ class attachVlanToGw(Action):
             net_connect = ConnectHandler(**opt)
             for cmd in cli_cmd:
                 cmd = cmd.strip()
-                cli_output[cmd] = (net_connect.send_command(cmd,expect_string='config'))
+                cli_output[cmd] = (net_connect.send_command(cmd, expect_string='config'))
                 self.logger.info('successfully executed cli %s', cmd)
             return True
 
-        except Exception as e:
-            self.logger.error(
-                'Execution of command: %s Failed with Exception: %s' % (e, cmd))
+        except RuntimeError as e:
+            self.logger.error('Execution of command: %s Failed with Exception: %s', e, cmd)
             return False
         finally:
             if net_connect is not None:

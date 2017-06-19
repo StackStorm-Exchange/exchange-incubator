e8b414cc6e9c907f2093ca4e398766a35f176f88
Anthony Shaw <anthonyshaw@apache.org>
2017-06-19 11:23:48 +1000

    undo all that lot

--- exchange/actions/base/__init__.py
+++
@@ -1,6 +0,0 @@
-from st2actions.runners.pythonrunner import Action
-
-
-class BaseExchangeAction(Action):
-    def __init__(self):
-        self.account = None
--- exchange/actions/get_folder.py
+++
@@ -1,8 +0,0 @@
-from lib import BaseExchangeAction
-
-
-class GetFolderAction(BaseExchangeAction):
-    def run(self, folder_name):
-        result = self.account.root.get_folder_by_name(name)
-
-        return result

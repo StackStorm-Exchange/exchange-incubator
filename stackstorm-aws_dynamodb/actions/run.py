69d2de4ee8b4ca2c54f52eede1137a0eaa24b5f9
Andy Moore <andy@impulsed.net>
2017-10-23 19:04:02 +0100

    stackstorm-aws_dynamodb

---
+++ stackstorm-aws_dynamodb/actions/run.py
@@ -0,0 +1,24 @@
+import st2aws
+
+from st2aws import action
+
+
+class ActionManager(action.BaseAction):
+
+    def run(self, **kwargs):
+        action = kwargs['action']
+        del kwargs['action']
+        module_path = kwargs['module_path']
+        del kwargs['module_path']
+        if action == 'run_instances':
+            kwargs['user_data'] = self.st2_user_data()
+        if action == 'create_tags':
+            kwargs['tags'] = self.split_tags(kwargs['tags'])
+        if action in ('add_a', 'update_a'):
+            kwargs['value'] = kwargs['value'].split(',')
+        if 'cls' in kwargs.keys():
+            cls = kwargs['cls']
+            del kwargs['cls']
+            return self.do_method(module_path, cls, action, **kwargs)
+        else:
+            return self.do_function(module_path, action, **kwargs)

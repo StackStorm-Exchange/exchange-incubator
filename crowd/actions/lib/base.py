9f57a615b395d6dcaf3d3806e9534ef39861cc41
Mohan L <thefossgeek@gmail.com>
2018-10-24 13:57:15 +0530

    Action library shared by other actions.

--- crowd/actions/lib/base.py
+++ crowd/actions/lib/base.py
@@ -1,2 +1,21 @@
+import crowd
 from st2common.runners.base_action import Action
 
+class CrowdBaseAction(Action):
+
+	def __init__(self, config):
+		super(CrowdBaseAction, self).__init__(config)
+		self.crowd = self._get_client()
+
+		# Test that application can authenticate to Crowd.
+		self.status = self.crowd.auth_ping()
+	
+	def _get_client(self):
+		crowd_configs = self.config['crowd']
+		app_url = crowd_configs['app_url']
+		app_user = crowd_configs['app_user']
+		app_pass = crowd_configs['app_pass']
+
+		# Create the reusable Crowd object.
+		client = crowd.CrowdServer(app_url, app_user, app_pass)
+		return client

3bf23aab71da0715080b6e12987ab44dfbc889cc
lm-ydubler <92544319+lm-ydubler@users.noreply.github.com>
2021-12-27 09:07:37 -0700

    Update sensor_constants.py

--- logicmonitor/sensors/sensor_constants.py
+++ logicmonitor/sensors/sensor_constants.py
@@ -9,21 +9,31 @@ ALERT_TRIGGER = "logicmonitor.alert_trigger"
 
 # Log constants
 LOG_LOADING_DATA = "PAYLOAD JSON LOADED SUCCESSFULLY!"
-LOG_FAILED_TO_PARSE_JSON = "DELIVERY NOT ACCEPTED! The webhook-sensor failed to parse the payload's body as JSON."
+LOG_FAILED_TO_PARSE_JSON = (
+    "DELIVERY NOT ACCEPTED! The webhook-sensor failed to parse the payload's body as JSON."
+)
 LOG_API_KEY_EXISTS = "PAYLOAD CONTAINS 'apiKey' FIELD!"
-LOG_API_KEY_DOES_NOT_EXIST = "DELIVERY NOT ACCEPTED! The payload does not contain a required object with key 'apiKey'."
+LOG_API_KEY_DOES_NOT_EXIST = (
+    "DELIVERY NOT ACCEPTED! The payload does not contain a required object with key 'apiKey'."
+)
 LOG_FAILED_BAD_RESPONSE = (
     "AUTHENTICATION FAILED! Webhook-sensor received bad response code from StackStorm: "
 )
 LOG_AUTH_ENABLED = "AUTHENTICATION IS ENABLED."
 LOG_AUTH_SUCCEEDED = "AUTHENTICATION SUCCEEDED!"
 LOG_AUTH_DISABLED = "AUTHENTICATION IS DISABLED! It is strongly recommended to enable authentication in your pack's configuration file!"
-LOG_TRIGGER_DISPATCHED = 'Trigger/payload dispatched into StackStorm. trigger="logicmonitor.alert_trigger" , payload='
+LOG_TRIGGER_DISPATCHED = (
+    'Trigger/payload dispatched into StackStorm. trigger="logicmonitor.alert_trigger" , payload='
+)
 LOG_DEFAULT_RESPONSE = "Unknown error has occurred."
 
 # HTTP Response Constants
-RES_FAILED_TO_PARSE_JSON = "DELIVERY NOT ACCEPTED! The webhook-sensor failed to parse the payload's body as JSON."
-RES_API_KEY_DOES_NOT_EXIST = "DELIVERY NOT ACCEPTED! The payload does not contain a required object with key 'apiKey'."
+RES_FAILED_TO_PARSE_JSON = (
+    "DELIVERY NOT ACCEPTED! The webhook-sensor failed to parse the payload's body as JSON."
+)
+RES_API_KEY_DOES_NOT_EXIST = (
+    "DELIVERY NOT ACCEPTED! The payload does not contain a required object with key 'apiKey'."
+)
 RES_FAILED_BAD_RESPONSE = (
     "AUTHENTICATION FAILED! Webhook-sensor received bad response code from StackStorm: "
 )
